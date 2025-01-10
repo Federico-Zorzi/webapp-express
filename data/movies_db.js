80e37a97458df35dd773b5f1099c760c27f65cbc
Federico-Zorzi <federico.zorzi05@gmail.com>
2025-01-10 16:04:36 +0100

    .env.example + index function

--- data/movies_db.js
+++ data/movies_db.js
@@ -15,5 +15,7 @@ const connection = mysql.createConnection({
 // RESPONSE FROM THE DB ABOUT CONNECTION
 connection.connect((err) => {
   if (err) throw err;
-  console.log("Connected to MySQL!");
+  /*   console.log("Connected to MySQL!"); */
 });
+
+module.exports = connection;
